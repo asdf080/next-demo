0138815670a8cec6997fc75ea2ec2f2a4f76f206
Dahee Lee <ai04120@naver.com>
2025-06-04 23:44:38 +0900

    fix: 스타일 설정

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import useProfileQuery from "@/api/use-profile-query";
-import Image from "next/image";
 
 export default function Profile() {
   const { data } = useProfileQuery();
   console.log(data);
 
   return (
-    <div>
-      <img src={data?.avatar_url} alt="profile" />
-      <div>name: {data?.login || "loading..."} </div>
-      <div>updated_at: {data?.updated_at}</div>
+    <div className="flex flex-col justify-center gap-4 p-10">
+      <div className="rounded-full overflow-hidden">
+        <img src={data?.avatar_url} alt="profile" className="rounded-full" />
+      </div>
+      <div className="text-2xl font:bold">name: {data?.login || "loading..."} </div>
+      <div className="text-xl">updated_at: {data?.updated_at}</div>
     </div>
   );
 }
